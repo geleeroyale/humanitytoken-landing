2cf960ee17e5c6f20081ddf7d1243a5f49ef5e9c
geleeroyale <krassvs@gmail.com>
2018-11-23 00:46:09 +0100

    Still playing with content

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -7,6 +7,7 @@ import Banner from "../components/Banner";
 import pic01 from "../assets/images/pic01.jpg";
 import pic02 from "../assets/images/pic02.jpg";
 import pic03 from "../assets/images/pic03.jpg";
+import pic04 from "../assets/images/pic04.jpg";
 
 const Landing = props => (
   <Layout>
@@ -20,22 +21,12 @@ const Landing = props => (
     <div id="main">
       <section id="one">
         <div className="inner">
-          <header className="major">
-            <h2>New Economies</h2>
-          </header>
           <p>
             <em>
               “A first step towards creating an incentivized micro-economy,
               distributing the means for people in unfavorable conditions to
               help themselves and build up their lives.”
             </em>
-          </p>
-          <p>
-            Human Token is a technical approach to solving issues of
-            distribution of basic social needs for people in need of help. The
-            project aims to alleviate trust-issues around social impact funds
-            and bridge the gap between the largely non-technicized, very poor
-            citizens and the evolution of modern services by automatization.
           </p>
         </div>
       </section>
@@ -47,19 +38,20 @@ const Landing = props => (
           <div className="content">
             <div className="inner">
               <header className="major">
-                <h3>Orci maecenas</h3>
+                <h3>New Economies</h3>
               </header>
               <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                magna sed nunc rhoncus condimentum sem. In efficitur ligula tate
-                urna. Maecenas massa sed magna lacinia magna pellentesque lorem
-                ipsum dolor. Nullam et orci eu lorem consequat tincidunt.
-                Vivamus et sagittis tempus.
+                Human Token is a technical approach to solving issues of
+                distribution of basic social needs for people in need of help.
+                The project aims to alleviate trust-issues around social impact
+                funds and bridge the gap between the largely non-technicized,
+                very poor citizens and the evolution of modern services by
+                automatization.
               </p>
               <ul className="actions">
                 <li>
                   <Link to="/generic" className="button">
-                    Learn more
+                    Cause, Effect, Solution
                   </Link>
                 </li>
               </ul>
@@ -68,24 +60,24 @@ const Landing = props => (
         </section>
         <section>
           <Link to="/generic" className="image">
-            <img src={pic02} alt="" />
+            <img src={pic04} alt="" />
           </Link>
           <div className="content">
             <div className="inner">
               <header className="major">
-                <h3>Rhoncus magna</h3>
+                <h3>Experimentation in Phases</h3>
               </header>
               <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                magna sed nunc rhoncus condimentum sem. In efficitur ligula tate
-                urna. Maecenas massa sed magna lacinia magna pellentesque lorem
-                ipsum dolor. Nullam et orci eu lorem consequat tincidunt.
-                Vivamus et sagittis tempus.
+                In a best case scenario, the experiment gets larger and the
+                reward for participation increases over time in a way that
+                allows the participants to climb Maslow’s hierarchy of needs at
+                least out of the lowest area and ideally to a point where they
+                even can contribute back to the system.
               </p>
               <ul className="actions">
                 <li>
                   <Link to="/generic" className="button">
-                    Learn more
+                    Roadmap
                   </Link>
                 </li>
               </ul>
@@ -98,16 +90,25 @@ const Landing = props => (
           </Link>
           <div className="content">
             <div className="inner">
-              <header className="major">
-                <h3>Sed nunc ligula</h3>
-              </header>
-              <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                magna sed nunc rhoncus condimentum sem. In efficitur ligula tate
-                urna. Maecenas massa sed magna lacinia magna pellentesque lorem
-                ipsum dolor. Nullam et orci eu lorem consequat tincidunt.
-                Vivamus et sagittis tempus.
-              </p>
+              <h3>Why Blockchain?</h3>
+              <ul>
+                <li>
+                  Valuable data can be gathered from analysing supply, demand,
+                  acceptance
+                </li>
+                <li>Records can be kept transparent</li>
+                <li>Donations can be tracked transparently</li>
+                <li>
+                  Creation of virtual ecosystem that can provide means for
+                  people in need to reintegrate to society
+                </li>
+                <li>Cooperation with similar minded initiatives</li>
+                <li>
+                  A hard topic, but when it comes to helping those in need, many
+                  people are simply afraid of interaction. What happens if
+                  trustless mechanisms establish a secure way to transact?
+                </li>
+              </ul>
               <ul className="actions">
                 <li>
                   <Link to="/generic" className="button">
