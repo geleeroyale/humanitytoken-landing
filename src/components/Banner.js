14cd6fec1f8978ca655b4547e196cdcb72c7d479
geleeroyale <krassvs@gmail.com>
2018-11-23 00:03:24 +0100

    Playing with content display types

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import logo from '../assets/images/humanity-token-logo.png'
+import React from "react";
+import logo from "../assets/images/humanity-token-logo.png";
 
 const Banner = props => (
   <section id="banner" className="major">
     <div className="inner">
       <header className="major">
-        <img src={logo} style={{ maxWidth: `200px` }} />
+        <img src={logo} style={{ maxWidth: `250px` }} />
       </header>
       <div className="content">
         <p>Tokenize Social Impact</p>
         <ul className="actions">
           <li>
             <a href="#one" className="button next scrolly">
-              Info
+              ⏀
             </a>
           </li>
         </ul>
       </div>
     </div>
   </section>
-)
+);
 
-export default Banner
+export default Banner;
