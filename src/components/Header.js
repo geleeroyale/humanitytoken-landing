84b15c47dd90fcf63060e77a1c227b58d66a8909
geleeroyale <krassvs@gmail.com>
2018-11-23 23:21:40 +0100

    color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
 const Header = props => (
   <header id="header" className="alt">
     <Link to="/" className="logo">
-      <strong>Humanity</strong> <span>Token</span>
+      <strong>Humanity Token</strong>
     </Link>
     <nav>
       <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">
@@ -13,10 +13,10 @@ const Header = props => (
       </a>
     </nav>
   </header>
-)
+);
 
 Header.propTypes = {
-  onToggleMenu: PropTypes.func,
-}
+  onToggleMenu: PropTypes.func
+};
 
-export default Header
+export default Header;
